de851a3a18f1a299d605f3f7e0c272987bc5fd01
GDI <dimon.1982@mail.ru>
2022-08-09 21:56:35 +0300

    lesson 2

---
+++ frontend/src/components/User.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+
+const UserItem = ({user}) => {
+    return (
+        <tr>
+            <td>
+                {user.first_name}
+            </td>
+            <td>
+                {user.last_name}
+            </td>
+            <td>
+                {user.e_mail}
+            </td>
+        </tr>
+    )
+}
+
+const UserList = ({users}) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        First name
+                    </th>
+                    <th>
+                        Last Name
+                    </th>
+                    <th>
+                        E-mail
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                    {users.map((user) => <UserItem user={user} />)}
+            </tbody>
+        </table>
+    )
+}
+
+export default UserList
