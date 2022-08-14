610c87fa94f3d6b480f529c7474e9f7d144c9072
GDI <dimon.1982@mail.ru>
2022-08-14 22:07:04 +0300

    lesson 3 add RENDER & PARSER in settings

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -129,3 +129,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES':[
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES':[
+        'rest_framework.parsers.JSONParser',
+    ]
+}
