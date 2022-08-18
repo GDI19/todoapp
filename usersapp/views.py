cbecedc9eddb94a5fd4874e39be057fe88ca4fb2
GDI <dimon.1982@mail.ru>
2022-08-18 17:34:36 +0300

    lesson 4

--- usersapp/views.py
+++ usersapp/views.py
@@ -1,9 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import TodoUser
 from .serializers import UserModelSerializer
+from rest_framework import mixins, viewsets
 
 
-class UserModelViewSet(ModelViewSet):
+class UserCustomViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
     queryset = TodoUser.objects.all()
     serializer_class = UserModelSerializer
 
