3e9894f1c72df9c6f10d804fffdd0f1d11dcfbb7
GDI <dimon.1982@mail.ru>
2022-08-22 19:43:12 +0300

    lesson 5

---
+++ frontend/src/components/NotFound404.js
@@ -0,0 +1,9 @@
+const NotFound404 = ({ location }) => {
+    return (
+        <div>
+            <h1>Страница по адресу '{location.pathname}' не найдена</h1>
+        </div>
+    )
+}
+
+export default NotFound404
