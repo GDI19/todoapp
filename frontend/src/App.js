c020fab2faa1905f63616fb945e56d5a9ed85e93
GDI <dimon.1982@mail.ru>
2022-08-24 20:02:38 +0300

    lesson 5 useless try

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
         this.state = {
             'users': [],
             'projects': [],
-            'todos':[]
+            'todos': []
         }
     }
 
--- frontend/src/components/NotFound404.js
+++ frontend/src/components/NotFound404.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 const NotFound404 = ({ location }) => {
     return (
         <div>
