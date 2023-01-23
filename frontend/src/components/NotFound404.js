c44f5197acd6f29127b2f982c19b72b72664efa2
GDI <dimon.1982@mail.ru>
2023-01-23 20:28:34 +0700

    1) deleted package. 2) added gitignore. 3) changed directories to push to the github all files after the incorrect preparation to docker

---
+++ frontend/src/components/NotFound404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound404 = ({ location }) => {
+    return (
+        <div>
+            <h1>Страница по адресу {location} не найдена</h1>
+        </div>
+    )
+}
+
+export default NotFound404;
